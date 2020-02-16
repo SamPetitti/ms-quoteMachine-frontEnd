068b2a7e234b6103db940fd002a4d3a6e27d759e
Samuel  Petitti <sampetitti@Samuels-MacBook-Pro-2.local>
2020-02-16 12:23:56 -0500

    get all quotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ms-quoteMachine-frontEnd';
+  response$: Observable<QuoteResponse>;
+  constructor(private client: HttpClient){}
+  
+
+  getQuotes(){
+    this.response$ = this.client.get<QuoteResponse>('localhost:5000/quotes/');
+    console.log(this.response$);
+  }
 }
+
+
+
+interface QuoteResponse{
+  data: Quote[]
+}
+
+interface Quote {
+  "id": number,
+  "text": "string",
+   "rating": number
+}
+
+// interface BooksResponse{
+//    data: BooksResponseItem[]
+// }
+
+// interface BooksResponseItem {
+//   id: number;
+//   title: string;
+//   author: string;
+// }
